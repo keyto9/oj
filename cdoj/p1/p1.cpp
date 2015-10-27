760964036d52b4c476cfde3142605576c8fa7445
dploop <dploop@163.com>
2015-10-27 14:09:07 +0800

    cdoj/p1

---
+++ cdoj/p1/p1.cpp
@@ -0,0 +1,17 @@
+/******************************************************************************
+Copyright (C), 2015, dploop
+FileName: p1.cpp
+Author: dploop
+Date: 2015/10/27
+Description: http://acm.uestc.edu.cn/#/problem/show/1
+******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	scanf("%d%d", &a, &b);
+	printf("%d\n", a + b);
+	return 0;
+}
+
