679ed0a0bb98752f61e504f26ee2c31d19b31bd5
dploop <dploop@163.com>
2015-10-28 11:43:44 +0800

    pythontip/p2

---
+++ pythontip/p2/p2.py
@@ -0,0 +1,10 @@
+###############################################################################
+## Copyright (C), 2015, dploop
+## FileName: p2.py
+## Author: dploop
+## Date: 2015/10/28
+## Description: http://www.pythontip.com/coding/code_oj_case/2
+###############################################################################
+
+print sorted(L)
+
