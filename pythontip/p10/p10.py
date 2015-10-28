79d6c9ff235fa7b7c1ad519429fb0109a32b4ec2
dploop <dploop@163.com>
2015-10-28 13:04:43 +0800

    pythontip/p9

---
+++ pythontip/p10/p10.py
@@ -0,0 +1,12 @@
+###############################################################################
+## Copyright (C), 2015, dploop
+## FileName: p10.py
+## Author: dploop
+## Date: 2015/10/28
+## Description: http://www.pythontip.com/coding/code_oj_case/10
+###############################################################################
+
+def gcd(a, b):
+    return gcd(b, a%b) if b else a
+
+print a*b/gcd(a, b)
