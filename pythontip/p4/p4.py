3f08742861c148c1efe8af063be7720f55a45778
dploop <dploop@163.com>
2015-10-28 12:00:49 +0800

    pythontip/p4

---
+++ pythontip/p4/p4.py
@@ -0,0 +1,10 @@
+###############################################################################
+## Copyright (C), 2015, dploop
+## FileName: p4.py
+## Author: dploop
+## Date: 2015/10/28
+## Description: http://www.pythontip.com/coding/code_oj_case/4
+###############################################################################
+
+print ",".join(a.keys())
+
