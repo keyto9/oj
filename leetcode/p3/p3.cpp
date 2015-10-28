72fc43aa8c699e1b5f2baccafb490b4b2e5338b9
dploop <dploop@163.com>
2015-10-28 16:49:10 +0800

    leetcode/p3

---
+++ leetcode/p3/p3.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+Copyright (C), 2015, dploop
+FileName: p3.cpp
+Author: dploop
+Date: 2015/10/28
+Description: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+******************************************************************************/
+class Solution {
+public:
+	int lengthOfLongestSubstring(string s) {
+		int n = s.length();
+		if (n <= 0) {
+			return 0;
+		}
+		int v[0x100]={0}, ret=1;
+		for (int i=0, j=0; j<n; ++j) {
+			for (++v[s[j]]; v[s[j]]>1; ) {
+				--v[s[i++]];
+			}
+			if (ret < (j-i+1)) {
+				ret = (j-i+1);
+			}
+		}
+		return ret;
+	}
+};
+
