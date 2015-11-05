5412b4d303897a2bf37142e52663333ee691b301
dploop <dploop@163.com>
2015-11-05 14:28:46 +0800

    leetcode/p239

---
+++ leetcode/p239/p239.cpp
@@ -0,0 +1,47 @@
+/******************************************************************************
+Copyright (C), 2015, dploop
+FileName: p239.cpp
+Author: dploop
+Date: 2015/11/5
+Description: https://leetcode.com/problems/sliding-window-maximum/
+******************************************************************************/
+class Solution {
+public:
+	class MyQue {
+	public:
+		deque< pair<int, int> > q;
+		int k;
+		MyQue(int ak): k(ak) {}
+		~MyQue() {}
+		void push(int index, int value) {
+			while (!q.empty()
+				&& (q.front().first+k <= index)) {
+				q.pop_front();
+			}
+			while (!q.empty()
+				&& (q.back().second <= value)) {
+				q.pop_back();
+			}
+			q.push_back(make_pair(index, value));
+		}
+		int query() {
+			return q.front().second;
+		}
+    };
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    	vector<int> ret;
+        MyQue mq(k);
+        int len = nums.size();
+        if (len <= 0 || len < k) {
+        	return ret;
+        }
+        for (int i = 0; i < k-1; ++i) {
+        	mq.push(i, nums[i]);
+        }
+        for (int i = k-1; i < len; ++i) {
+        	mq.push(i, nums[i]);
+        	ret.push_back(mq.query());
+        }
+        return ret;
+    }
+};
