b18f2630f1da310ba4238314a07957c52eba050c
dploop <dploop@163.com>
2015-11-05 14:22:22 +0800

    leetcode/p233

---
+++ leetcode/p233/p233.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+Copyright (C), 2015, dploop
+FileName: p233.cpp
+Author: dploop
+Date: 2015/11/5
+Description: https://leetcode.com/problems/number-of-digit-one/
+******************************************************************************/
+class Solution {
+public:
+	typedef long long int LL;
+    int countDigitOne(int n) {
+        int ret = 0;
+        for (LL b = 1, tb; b <= n; b = tb) {
+        	tb = b * 10;
+        	ret += n / tb * b;
+        	int m = n % tb - b + 1;
+        	m = m > 0 ? m : 0;
+        	m = m < b ? m : b;
+        	ret += m;
+        }
+        return ret;
+    }
+};
+
