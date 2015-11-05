de313d634d0bd19751ff78a2047f6db1bcbd1110
dploop <dploop@163.com>
2015-11-05 14:25:41 +0800

    leetcode/p235

---
+++ leetcode/p235/p235.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+Copyright (C), 2015, dploop
+FileName: p235.cpp
+Author: dploop
+Date: 2015/11/5
+Description: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+******************************************************************************/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return search(root, p, q);
+    }
+private:
+    TreeNode* search(TreeNode *parent, TreeNode *p, TreeNode *q) {
+        if (p->val < parent->val
+            && q->val < parent->val) {
+                return search(parent->left, p, q);
+            }
+        if (p->val > parent->val
+            && q->val > parent->val) {
+                return search(parent->right, p, q);
+            }
+        return parent;
+    }
+};
+
