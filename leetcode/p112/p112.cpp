349a5916e0f2d5380c7f9a226f9106b331fb2657
dploop <dploop@163.com>
2016-04-23 15:27:38 +0800

    foobar

---
+++ leetcode/p112/p112.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+Copyright (C), 2016, dploop
+FileName: p112.cpp
+Author: dploop
+Date: 2016/4/23
+Description: https://leetcode.com/problems/path-sum/
+******************************************************************************/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	bool hasPathSum(TreeNode* root, int sum) {
+		if (!root) {return false;}
+		return myPathSum(root, sum, 0);
+	}
+private:
+	bool myPathSum(TreeNode* root, int sum, int acc) {
+		if (!root->left && !root->right) {
+			return sum == acc + root->val;
+		}
+		if (!root->left) {
+			return myPathSum(root->right, sum, acc+root->val);
+		}
+		if (!root->right) {
+			return myPathSum(root->left, sum, acc+root->val);
+		}
+		return myPathSum(root->left, sum, acc+root->val)
+			|| myPathSum(root->right, sum, acc+root->val);
+	}
+};
+
