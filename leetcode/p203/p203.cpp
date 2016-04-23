9e461fed1aab600c786841fee44e8d9344f32956
dploop <dploop@163.com>
2016-04-23 18:14:05 +0800

    foobar

---
+++ leetcode/p203/p203.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+Copyright (C), 2016, dploop
+FileName: p203.cpp
+Author: dploop
+Date: 2016/4/23
+Description: https://leetcode.com/problems/remove-linked-list-elements/
+******************************************************************************/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode* removeElements(ListNode* head, int val) {
+		if (!head) return head;
+		ListNode *ptr = head, *tmp;
+		while (ptr->next) {
+			if (ptr->next->val == val) {
+				tmp = ptr->next;
+				ptr->next = tmp->next;
+				delete tmp;
+			} else {
+				ptr = ptr->next;
+			}
+		}
+		tmp = head->next;
+		if (head->val == val) {
+			delete head; return tmp;
+		} else {
+			return head;
+		}
+	}
+};
+
