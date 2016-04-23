cdabce7a0a3efa5e686703d7a2e751b503fb74f9
dploop <dploop@163.com>
2016-04-23 17:36:30 +0800

    foobar

---
+++ leetcode/p190/p190.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+Copyright (C), 2016, dploop
+FileName: p190.cpp
+Author: dploop
+Date: 2016/4/23
+Description: https://leetcode.com/problems/reverse-bits/
+******************************************************************************/
+class Solution {
+public:
+	uint32_t reverseBits(uint32_t n) {
+		n = (n >> 16) | (n << 16);
+		n = ((n & 0xff00ff00U) >> 8) | ((n & 0x00ff00ffU) << 8);
+		n = ((n & 0xf0f0f0f0U) >> 4) | ((n & 0x0f0f0f0fU) << 4);
+		n = ((n & 0xccccccccU) >> 2) | ((n & 0x33333333U) << 2);
+		n = ((n & 0xaaaaaaaaU) >> 1) | ((n & 0x55555555U) << 1);
+		return n;
+	}
+};
+
