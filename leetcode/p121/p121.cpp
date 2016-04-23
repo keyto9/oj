a62fadd73dd290756567e72d5db00c1a49e459f4
dploop <dploop@163.com>
2016-04-23 13:38:35 +0800

    foobar

---
+++ leetcode/p121/p121.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+Copyright (C), 2016, dploop
+FileName: p121.cpp
+Author: dploop
+Date: 2016/4/23
+Description: https://leetcode.com/problems/reverse-string/
+******************************************************************************/
+class Solution {
+public:
+	int maxProfit(vector<int>& prices) {
+		int min_price = INT_MAX, max_profit = 0;
+		for (auto curr_price : prices) {
+			min_price = min(min_price, curr_price);
+			max_profit = max(max_profit
+						, curr_price  -min_price);
+		}
+		return max_profit;
+	}
+};
+
