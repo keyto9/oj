f9949cbf2efafc429871fbe2be74ff9692424130
dploop <dploop@163.com>
2016-04-23 15:57:01 +0800

    foobar

---
+++ leetcode/p36/p36.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+Copyright (C), 2016, dploop
+FileName: p36.cpp
+Author: dploop
+Date: 2016/4/23
+Description: https://leetcode.com/problems/valid-sudoku/
+******************************************************************************/
+class Solution {
+public:
+	bool isValidSudoku(vector<vector<char>>& board) {
+		if (board.size() != 9) return false;
+		for (int i=0; i < 9; ++i) {
+			if (board[i].size() != 9) return false;
+			if (!isValidSquare(board, i, i+1, 0, 9))
+				return false;
+		}
+		for (int j=0; j < 9; ++j) {
+			if (!isValidSquare(board, 0, 9, j, j+1))
+				return false;
+		}
+		for (int i=0; i < 9; i +=3) {
+			for (int j=0; j < 9; j += 3) {
+				if (!isValidSquare(board, i, i+3, j, j+3))
+					return false;
+			}
+		}
+		return true;
+	}
+private:
+	bool isValidSquare(vector<vector<char>>& board
+		, int ib, int ie, int jb, int je) {
+		bool vst[9] = {false};
+		for (int i=ib; i < ie; ++i) {
+			for (int j=jb; j < je; ++j) {
+				char curr = board[i][j];
+				if ('1' <= curr && curr <= '9') {
+					int iurr = curr - '1';
+					if (vst[iurr]) return false;
+					vst[iurr] = true;
+				}
+			}
+		}
+		return true;
+	}
+};
+
