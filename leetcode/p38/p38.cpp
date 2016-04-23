62b7fc1ed63da7204d44322c65766d7dac1eb28d
dploop <dploop@163.com>
2016-04-23 18:03:59 +0800

    foobar

---
+++ leetcode/p38/p38.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+Copyright (C), 2016, dploop
+FileName: p38.cpp
+Author: dploop
+Date: 2016/4/23
+Description: https://leetcode.com/problems/count-and-say/
+******************************************************************************/
+class Solution {
+public:
+	string countAndSay(int n) {
+		string ret = "1";
+		for (int i=2; i <= n; ++i) {
+			ret = next(ret);
+		}
+		return ret;
+	}
+private:
+	string next(string input) {
+		string ret = ""; input += '$';
+		char c = '^'; int t = 0;
+		for (char x : input) {
+			if (x != c) {
+				ret += fmt(t, c);
+				c = x; t = 1;
+			} else {
+				++t;
+			}
+		}
+		return ret;
+	}
+	string fmt(int t, char c) {
+		char buff[23] = {'\0'};
+		if (t > 0) {
+			sprintf(buff, "%d%c", t, c);
+		}
+		return buff;
+	}
+};
+
