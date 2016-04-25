59978f60af5596c0a49880a019a260e85f3ab7c6
dploop <dploop@163.com>
2016-04-25 17:18:13 +0800

    foo

--- leetcode/p5/p5.cpp
+++ leetcode/p5/p5.cpp
@@ -7,31 +7,23 @@ Description: https://leetcode.com/problems/longest-palindromic-substring/
 ******************************************************************************/
 class Solution {
 private:
-	bool equal(string &T, int n, int x, int y) {
-		if (x < 0 || x > 2*n) return false;
-		if (y < 0 || y > 2*n) return false;
-		int ox = x&1, oy = y&1;
+	bool eq(string &T, int n, int x, int y) {
+		if (x < 0 || x > n) return false;
+		if (y < 0 || y > n) return false;
+		int ox = x & 1, oy = y & 1;
 		return ((ox && oy)
-		? T[x/2]==T[y/2] : !(ox^oy));
+			? T[x/2]==T[y/2] : !(ox^oy));
 	}
 public:
 	string longestPalindrome(string &T) {
-		int n=T.length(); vector<int> P(2*n, 0);
-		int C = 0, R = 0, MC = 0, MR = 0;
-		for (int i = 1; i < 2*n; ++i) {
-			int K = 0; if (i < R) {
-				K = min(R-i, P[2*C-i]);
-			}
-			for (; equal(T, n, i-K-1, i+K+1); ) {
-				++K;
-			}
-			if (i + K > R) {
-				C = i; R = i + K;
-			}
-			if (MR - MC < K) {
-				MC = C; MR = R;
-			}
-			P[i] = K;
+		int n = 2*T.length(); vector<int> P(n);
+		int C = 0, R = 0, MC = 0, MR = 0, K = 0;
+		for (int i = 1; i < n; ++i) {
+			if (i < R) {K = min(R-i,P[2*C-i]);}
+			for (; eq(T,n,i-K-1,i+K+1); ++K);
+			if (i + K > R) {C = i; R = i + K;}
+			if (MR - MC < K) {MC = C; MR = R;}
+			P[i] = K; K = 0;
 		}
 		return T.substr((2*MC-MR)/2, MR-MC);
 	}
